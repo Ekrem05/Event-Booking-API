6a9320bac4409914c6942a9b96c7718d61f40969
Ekrem Beytula <78847462+Ekrem05@users.noreply.github.com>
2024-08-31 18:02:05 +0300

    First endpoint

--- main.go
+++ main.go
@@ -5,6 +5,12 @@ import (
 )
 func main() {
 	server:=gin.Default()
-
+	server.GET("/",index)
 	server.Run(":8080")
+}
+
+func index(context *gin.Context) {
+  
+	body:=map[string]string{"message":"Hello World!"}
+	context.JSON(200,body);
 }
